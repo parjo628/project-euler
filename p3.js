a5629cade11ccc9387822b31eaf66b1200dd7985
parjo628 <parjo628@outlook.com>
2015-11-16 20:50:17 +1300

    euler 3 updated

--- p3.js
+++ p3.js
@@ -3,6 +3,7 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 
+//the function below is just to check if a number is prime. Not part of the answer.
 function isPrime(num) {
 	for (var i=2; i<num; i++) {
 		if (num % i === 0)
@@ -11,6 +12,7 @@ function isPrime(num) {
 	return true;
 }	
 
+//This is the answer.
 function primeFactor(num) {
 	var x = num;
 	for (var i = 2; i < Math.sqrt(num); i++)
